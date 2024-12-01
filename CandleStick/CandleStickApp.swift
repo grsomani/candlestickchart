4bc606f90adddc5a9209bc76085b7acae86a3ae7
Ganesh Somani <ganesh.somani@gmail.com>
2024-12-01 20:48:52 +0530

    Initial Commit

---
+++ CandleStick/CandleStickApp.swift
@@ -0,0 +1,17 @@
+//
+//  CandleStickApp.swift
+//  CandleStick
+//
+//  Created by Ganesh on 01/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CandleStickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
