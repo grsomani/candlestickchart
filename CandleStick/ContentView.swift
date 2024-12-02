e196b9daa0c9e125cda933e6b715b35f64944b80
Ganesh Somani <ganesh.somani@gmail.com>
2024-12-02 20:56:20 +0530

    Added Y selection

--- CandleStick/ContentView.swift
+++ CandleStick/ContentView.swift
@@ -10,58 +10,92 @@ import Charts
 
 struct PriceData: Identifiable {
     let id = UUID()
-    var day: Int
+    var timePeriod: Date
     var openPrice: Double
     var closePrice: Double
     var high: Double
     var low: Double
 }
 
+let currentTime = Date.now
+let calendar = Calendar.current
+
 var priceData: [PriceData] = [
-    .init(day: 1, openPrice: 10, closePrice: 20, high: 25, low: 5),
-    .init(day: 2, openPrice: 22, closePrice: 20, high: 20, low: 15),
-    .init(day: 3, openPrice: 20, closePrice: 25, high: 25, low: 10),
-    .init(day: 4, openPrice: 23, closePrice: 30, high: 35, low: 15)
+    .init(timePeriod: calendar.date(byAdding: .hour, value: -4, to: currentTime)!,
+          openPrice: 10,
+          closePrice: 20,
+          high: 25,
+          low: 5),
+    .init(timePeriod: calendar.date(byAdding: .hour, value: -3, to: currentTime)!,
+          openPrice: 22,
+          closePrice: 20,
+          high: 20,
+          low: 15),
+    .init(timePeriod: calendar.date(byAdding: .hour, value: -2, to: currentTime)!,
+          openPrice: 20,
+          closePrice: 25,
+          high: 25,
+          low: 10),
+    .init(timePeriod: calendar.date(byAdding: .hour, value: -1, to: currentTime)!,
+          openPrice: 23,
+          closePrice: 30,
+          high: 35,
+          low: 15),
+    .init(timePeriod: currentTime,
+          openPrice: 12,
+          closePrice: 25,
+          high: 25,
+          low: 5),
+    .init(timePeriod: calendar.date(byAdding: .hour, value: 1, to: currentTime)!,
+          openPrice: 22,
+          closePrice: 20,
+          high: 20,
+          low: 15),
+    .init(timePeriod: calendar.date(byAdding: .hour, value: 2, to: currentTime)!,
+          openPrice: 20,
+          closePrice: 25,
+          high: 25,
+          low: 15),
+    .init(timePeriod: calendar.date(byAdding: .hour, value: 3, to: currentTime)!,
+          openPrice: 25,
+          closePrice: 31,
+          high: 32,
+          low: 17)
 ]
 
 
 struct ContentView: View {
-    
-    @State private var currentZoom = 0.0
-    @State private var totalZoom = 1.0
+    @State private var selectedPrice: Double?
     
     var body: some View {
-        NavigationStack {
+        NavigationView {
             Chart {
                 ForEach(priceData) { item in
                     let chartColor: Color = item.closePrice > item.openPrice ? .green : .red
-                    RectangleMark(x: .value("Day", item.day),
-                                  yStart: .value("Low Price", item.openPrice),
-                                  yEnd: .value("High Price", item.closePrice),
+                    RectangleMark(x: .value("Day", item.timePeriod),
+                                  yStart: .value("Open", item.openPrice),
+                                  yEnd: .value("Close", item.closePrice),
                                   width: 4)
                     .foregroundStyle(chartColor)
                     
-                    RectangleMark(x: .value("Day", item.day),
-                                  yStart: .value("Low Price", item.low),
-                                  yEnd: .value("High Price", item.high),
+                    RectangleMark(x: .value("Day", item.timePeriod),
+                                  yStart: .value("Low", item.low),
+                                  yEnd: .value("High", item.high),
                                   width: 1)
                     .foregroundStyle(chartColor)
                 }
+                
+                if (selectedPrice != nil) {
+                    RuleMark(y: .value("Price", selectedPrice ?? 0.0))
+                        .foregroundStyle(Color.black)
+                        .annotation {
+                            Text("\(selectedPrice ?? 0.0, specifier: "%.2f")")
+                        }
+                }
             }
-            .padding(20)
-            .scaleEffect(currentZoom + totalZoom < 1 ? 1 : currentZoom + totalZoom)
-            .gesture(
-                MagnifyGesture()
-                    .onChanged { value in
-                        currentZoom = value.magnification - 1
-                    }
-                    .onEnded { value in
-                        totalZoom += currentZoom
-                        currentZoom = 0
-                    }
-            )
+            .chartYSelection(value: $selectedPrice)
+            .navigationBarTitle("Charts")
         }
-        .navigationTitle("Charts")
     }
 }
 
